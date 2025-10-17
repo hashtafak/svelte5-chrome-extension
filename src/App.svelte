1a0706767d427329f79e63bdace8b48ea99f6d15
trentbrew <hello@trentbrew.com>
2025-10-17 10:34:54 -0700

    ui: bottom dock

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { theme, initializeTheme } from "./lib/stores/theme";
+  import { onMount } from 'svelte';
+  import { theme, initializeTheme } from './lib/stores/theme';
 
-  import ThemeToggle from "./lib/components/ThemeToggle.svelte";
-  import Sidebar from "./lib/components/Sidebar.svelte";
-  import MainContent from "./lib/components/MainContent.svelte";
+  import ThemeToggle from './lib/components/ThemeToggle.svelte';
+  import Nav from './lib/components/Nav.svelte';
+  import MainContent from './lib/components/MainContent.svelte';
 
   onMount(() => {
     initializeTheme();
@@ -13,12 +13,12 @@
 
 <div class="h-screen bg-base-100" data-theme={$theme}>
   <div class="flex h-full">
-    <Sidebar />
     <main class="flex-1 p-4">
       <div class="flex justify-end mb-4">
         <ThemeToggle />
       </div>
       <MainContent />
     </main>
+    <Nav />
   </div>
 </div>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { MENU_ITEMS } from "../constants/menu";
+  import { MENU_ITEMS } from '../constants/menu';
 </script>
 
-<aside class="w-16 bg-base-200 p-2">
-  <nav class="flex flex-col gap-4">
+<aside class="h-16 bg-base-200 p-2 w-full bottom-0 fixed">
+  <nav class="flex flex-row gap-4 justify-center">
     {#each MENU_ITEMS as item}
       <button class="btn btn-ghost btn-square" onclick={item.action}>
         <span class="text-xl" title={item.label}>{item.icon}</span>
